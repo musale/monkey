170ebf1a7ce279bb32c89150e7e8b2d8016b7184
Martin Musale <martinmshale@gmail.com>
2020-05-15 10:08:27 +0300

    Revamp the REPL

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -6,15 +6,30 @@ import (
 	"io"
 
 	"github.com/musale/monkey/pkg/lexer"
-	"github.com/musale/monkey/pkg/token"
+	"github.com/musale/monkey/pkg/parser"
 )
 
 // PROMPT will appear at the start of a statement in the REPL
 const PROMPT = ">> "
 
+// monkeyFace is the guy
+const monkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 // Start will fire up a repl
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	fmt.Fprintf(out, monkeyFace)
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -27,9 +42,21 @@ func Start(in io.Reader, out io.Writer) {
 			return
 		}
 		l := lexer.NewLexer(line)
+		p := parser.NewParser(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, err := range errors {
+		io.WriteString(out, fmt.Sprintf("\t%s\n", err))
 	}
 }
--- pkg/repl/repl_test.go
+++ pkg/repl/repl_test.go
@@ -19,29 +19,34 @@ func (w *writerSpy) Write(p []byte) (n int, err error) {
 }
 
 func TestStart(t *testing.T) {
-	t.Run("Count exactly 1 call for PROMPT", func(t *testing.T) {
+	t.Run("Count exactly 2 calls for PROMPT and monkeyFace", func(t *testing.T) {
 		input := strings.NewReader("")
-		out := &writerSpy{}
-		calls := 1
-		Start(input, out)
-		compareInts(t, out.counter, calls)
-	})
-
-	t.Run("Count exactly 2 calls for PROMPT and exit", func(t *testing.T) {
-		input := strings.NewReader("exit")
 		out := &writerSpy{}
 		calls := 2
 		Start(input, out)
 		compareInts(t, out.counter, calls)
 	})
 
-	t.Run("Count 14 calls to print tokens and 2 for the PROMPT", func(t *testing.T) {
-		expect := "let add = fn(x, y){x + y}"
-		input := strings.NewReader(expect)
-
+	t.Run("Count exactly 3 calls for PROMPT, monkeyFace and exit", func(t *testing.T) {
+		input := strings.NewReader("exit")
 		out := &writerSpy{}
-		// 14 calls to token and 2 calls to PROMPT
-		calls := 16
+		calls := 3
+		Start(input, out)
+		compareInts(t, out.counter, calls)
+	})
+
+	t.Run("Count exactly 5 calls for PROMPT, monkeyFace and statement", func(t *testing.T) {
+		input := strings.NewReader("let x = 5;")
+		out := &writerSpy{}
+		calls := 5
+		Start(input, out)
+		compareInts(t, out.counter, calls)
+	})
+
+	t.Run("Count exactly 4 calls for PROMPT, monkeyFace and errors", func(t *testing.T) {
+		input := strings.NewReader("& error")
+		out := &writerSpy{}
+		calls := 4
 		Start(input, out)
 		compareInts(t, out.counter, calls)
 	})
